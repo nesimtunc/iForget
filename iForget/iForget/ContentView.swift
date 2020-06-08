ad31d416e5d75b2526ebb0087a19ecb8f1d9b852
Nesim TUNC <nesimtunc@gmail.com>
2020-06-08 10:43:59 +0900

    Created the project

---
+++ iForget/iForget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iForget
+//
+//  Created by Neso on 2020/06/08.
+//  Copyright © 2020 Rainlab. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
