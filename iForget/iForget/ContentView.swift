75a7e0cc148cf8e1b754cf02063cfd932fe2b255
Nesim TUNC <nesimtunc@gmail.com>
2020-06-08 11:31:57 +0900

    Created UI for notification

--- iForget/iForget/ContentView.swift
+++ iForget/iForget/ContentView.swift
@@ -9,8 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+
+	@State var message = UserDefaults.standard.string(forKey: "message") ?? ""
+	@State var result = ""
+
     var body: some View {
-        Text("Hello, World!")
+		VStack {
+			Form {
+				Section (header: Text("Message")
+					.font(Font.system(size: 50, weight: .medium, design: .rounded))
+					, content: {
+
+						// Two way binding $message
+						TextField("Your message", text: self.$message)
+							.frame(minWidth: 320, maxWidth: .infinity, minHeight: 100, alignment: .topLeading)
+
+						Button("Save",
+							   action: {
+								// let's save it in UserDefaults
+								UserDefaults.self.standard.setValue(self.message, forKey: "message")
+								self.result = "Saved!"
+
+						}).font(.largeTitle)
+							.padding(.top, 16)
+							.padding(.bottom, 16)
+				})
+
+			}.frame(minWidth: 320, maxWidth: .infinity, minHeight: 250, maxHeight: 250, alignment: .topLeading)
+
+			Text(self.result)
+			Spacer()
+		}
     }
 }
 
